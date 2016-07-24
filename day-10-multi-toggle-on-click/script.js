df184f54e289ba3712307eedf00be46ca1ce22f9
Tim Roberts <tmroberts7@gmail.com>
2016-07-24 11:08:24 -0500

    Updating functions since document.querySelectorAll iteration failed

--- day-10-multi-toggle-on-click/script.js
+++ day-10-multi-toggle-on-click/script.js
@@ -1,13 +1,37 @@
+/*
+I cannot get a solution that iterates across the array returned from querySelectorAll . . .
+SOOOOO...  I'm hard-coding the variables/eventListeners for each name/circle just to finish.  bleh... ask Eric on Monday
+*/
+/* vars for each name to be clicked */
 var bob = document.querySelector('.name_bob');
+var sue = document.querySelector('.name_sue');
+var fred = document.querySelector('.name_fred');
+var jack = document.querySelector('.name_jack');
+var dean = document.querySelector('.name_dean');
 
-function onClass() {
-  var containsClass = bob.classList.contains('on');
-  if (containsClass === false) {
-    bob.classList.add('on');
-  }
-  else {
-    bob.classList.remove('on');
-  }
+/* toggle functions */
+function toggleBob() {
+    bob.classList.toggle('on');
 }
-      
-bob.addEventListener('click', onClass);
+
+function toggleSue() {
+    sue.classList.toggle('on');
+}
+
+function toggleFred() {
+    fred.classList.toggle('on');
+}
+
+function toggleJack() {
+    jack.classList.toggle('on');
+}
+
+function toggleDean() {
+    dean.classList.toggle('on');
+}
+/* Listeners */
+bob.addEventListener('click', toggleBob);
+sue.addEventListener('click', toggleSue);
+fred.addEventListener('click', toggleFred);
+jack.addEventListener('click', toggleJack);
+dean.addEventListener('click', toggleDean);
