31ac9f250623a96fcb94546f8cbc04777550cafe
Tim Roberts <tmroberts7@gmail.com>
2016-08-02 17:00:17 -0500

    Updates to error handling and messaging

--- day-17-guess-the-number/script.js
+++ day-17-guess-the-number/script.js
@@ -1,28 +1,49 @@
 var button = document.querySelector('#button');
 var input = document.querySelector('#input');
 var output = document.querySelector('.output');
+var inputValue = '';
+var randomNumber = Math.ceil(Math.random() * 50);
+var tries = 0;
 
-var inputValue;
-
-
-var randomNumber = Math.ceil(Math.random() * 10);
-
+var used = document.querySelector('.used');
+var numbersUsed = [];
 
 function guessFunc() {
+    inputValue = input.value;
 
-inputValue = input.value;
+    if (isNaN(inputValue)) {
+      output.textContent = 'hey dufus... Enter a number. This is a number game';
+      return;
+    }
 
-     if(inputValue < randomNumber) {
-       output.textContent = "higher";
-     }
-     else if(inputValue > randomNumber) {
-       output.textContent = "lower";
-     }
-     else {
-       output.textContent = "You win!";
-     }
+    if (inputValue == '') {
+      output.textContent = 'You did not enter anything. This is a number game';
+      return;
+    }
 
+    if (inputValue > 50 ) {
+      output.textContent = 'Follow directions. . .  Enter a number less than 50';
+      return;
+    }
+
+    if(inputValue < randomNumber) {
+      output.textContent = "higher";
+    }
+    else if(inputValue > randomNumber) {
+      output.textContent = "lower";
+    }
+    else {
+      output.textContent = "You win!";
+    };
+
+    numbersUsed.push(inputValue);
+    used.textContent = numbersUsed;
+
+    tries +=1;
+    if (tries == 5) {
+      output.textContent = "You have run out of guesses!";
+      return;
+    };
 };
-
 
 button.addEventListener('click', guessFunc);
